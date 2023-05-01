d179ce42480046e85ca1ed763f2f9d683956e47f
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-05-01 10:55:38 -0300

    configurando produtor para objeto json

--- payment-service/src/main/java/io/github/AugustoMello09/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/io/github/AugustoMello09/services/impl/PaymentServiceImpl.java
@@ -1,18 +1,32 @@
 package io.github.AugustoMello09.services.impl;
 
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import io.github.AugustoMello09.model.Payment;
 import io.github.AugustoMello09.services.PaymentService;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService{
-
+	
+	@Autowired
+	private KafkaTemplate<String, Serializable> kafkaTemplate;
+	
+	@SneakyThrows
 	@Override
 	public void sendPayment(Payment payment) {
-		log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);	
+		log.info("PAYMENT_SERVICE_IMPL Recebi o pagamento {}", payment);
+		log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
 	}
 
 }
